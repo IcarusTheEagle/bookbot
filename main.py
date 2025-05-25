97c15dc71e93ea21f21166b9c885958ec2a42226
IcarusTheEagle <jacobchoate051@gmail.com>
2025-05-24 19:27:45 -0500

    imported sys and replaced sys.argv[1] with hard-code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from stats import word_count
 from stats import char_count
 from stats import sort_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -8,7 +13,7 @@ def get_book_text(file_path):
     return contents
 
 def main():
-    return get_book_text("books/frankenstein.txt")
+    return get_book_text(sys.argv[1])
 
 n = word_count(main())
 c = char_count(main())
